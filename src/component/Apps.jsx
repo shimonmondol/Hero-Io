a8e58631f605fcbb5215d8453d4c25b4c64fa137
Shimon Mondol <Shimonuap072@gmail.com>
2026-03-24 16:31:35 +0600

    responsive hero-apps

--- src/component/Apps.jsx
+++ src/component/Apps.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
-import { CiSearch } from "react-icons/ci";
 
 const Apps = () => {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetch("/public/appdata.json")
+    fetch("/appdata.json")
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
@@ -17,35 +16,50 @@ const Apps = () => {
 
   return (
     <div className="bg-[#F5F5F5] py-10">
-      <div className="max-w-6xl mx-auto pl-6">
-        <h1 className="text-center text-3xl font-bold">Our All Applications</h1>
-        <p className="text-center mt-3 text-gray-500">
+      <div className="max-w-6xl mx-auto px-4">
+        <h1 className="text-center text-2xl md:text-3xl font-bold">
+          Our All Applications
+        </h1>
+
+        <p className="text-center mt-3 text-gray-500 text-sm md:text-base">
           Explore All Apps on the Market developed by us. We code for Millions
         </p>
-        {/* Grid layout */}
-        <div className="flex justify-between mt-10">
-          ({filteredData.length}) Apps Found
+
+        {/* Top Section */}
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center mt-10 gap-4">
+          <h1 className="text-lg md:text-2xl font-bold">
+            ({filteredData.length}) Apps Found
+          </h1>
+
           <input
             type="text"
             placeholder="Search Apps"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            className="input border border-gray-300 focus:border-none focus:outline-none focus:ring-0 mr-3"
+            className="border border-gray-300 rounded-lg px-4 py-2 w-full md:w-72 focus:outline-none"
           />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 pt-6">
+
+        {/* Grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 pt-6">
           {filteredData.map((item) => (
             <div
               key={item.id}
-              className="bg-white w-full p-4 border border-gray-200 rounded-xl shadow-sm hover:shadow-lg transition"
+              className="bg-white p-4 border border-gray-200 rounded-xl shadow-sm hover:shadow-lg transition w-full"
             >
               <img
-                className="rounded-xl w-50 h-50 object-cover"
+                className="rounded-xl w-full h-40 md:h-48 object-cover"
                 src={item.image}
                 alt={item.title}
               />
-              <h5 className="mt-4 mb-2 text-lg font-semibold">{item.title}</h5>
-              <p className="text-sm text-gray-600">{item.description}</p>
+
+              <h5 className="mt-4 mb-2 text-base md:text-lg font-semibold">
+                {item.title}
+              </h5>
+
+              <p className="text-sm text-gray-600 line-clamp-2">
+                {item.description}
+              </p>
             </div>
           ))}
         </div>
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,66 +2,101 @@ import GooglePlay from "../assets/GooglePlay.png";
 import AppStore from "../assets/AppStore.png";
 import Hero from "../assets/hero.png";
 import { useNavigate } from "react-router";
+import { useEffect, useState } from "react";
 
 const Home = () => {
   const navigate = useNavigate();
+  const [apps, setApps] = useState([]);
+  const firstEightApps = apps.slice(0, 8);
+
+  useEffect(() => {
+    fetch("/appdata.json")
+      .then((res) => res.json())
+      .then((data) => setApps(data));
+  }, []);
+
   return (
     <div className="bg-[#F5F5F5]">
-      <div>
-        <h1 className="text-center text-4xl font-bold pt-12">
-          We Build <br />
-          <span className="text-purple-700 px-2 rounded leading-12">
-            Productive
-          </span>{" "}
-          Apps
-        </h1>
-        <p className="text-center text-gray-500 pt-4 px-2 text-sm sm:text-base leading-relaxed">
-          At HERO.IO, we craft innovative apps designed to make everyday life
-          simpler, smarter, and more exciting.
-          <br className="hidden sm:block" />
-          Our goal is to turn your ideas into digital experiences that truly
-          make an impact.
-        </p>
-        <div className="flex justify-center gap-3 pt-6">
-          <a href="https://play.google.com/store" target="_blank">
-            <img className="h-10" src={GooglePlay} alt="Google Play" />
-          </a>
+      <h1 className="text-center text-4xl font-bold pt-12">
+        We Build <br />
+        <span className="text-purple-700 px-2 rounded leading-12">
+          Productive
+        </span>{" "}
+        Apps
+      </h1>
+      <p className="text-center text-gray-500 pt-4 px-2 text-sm sm:text-base leading-relaxed">
+        At HERO.IO, we craft innovative apps designed to make everyday life
+        simpler, smarter, and more exciting.
+        <br className="hidden sm:block" />
+        Our goal is to turn your ideas into digital experiences that truly make
+        an impact.
+      </p>
+      <div className="flex justify-center gap-3 pt-6">
+        <a href="https://play.google.com/store" target="_blank">
+          <img className="h-10" src={GooglePlay} alt="Google Play" />
+        </a>
 
-          <a href="https://www.apple.com/app-store/" target="_blank">
-            <img className="h-10" src={AppStore} alt="App Store" />
-          </a>
-        </div>
-        <div className="flex justify-center items-center text-center pt-10">
-          <img className="h-40 lg:h-80" src={Hero} alt="" />
-        </div>
-        <div className="bg-purple-600 text-center text-white pb-10">
-          <h1 className="font-semibold text-2xl pt-10">
-            Trusted by Millions, Built for You
-          </h1>
-          <div className="flex flex-col sm:flex-row text-center justify-center gap-10 sm:gap-36 pt-10">
-            <div>
-              <p className="text-[10px]">Total Downloads</p>
-              <h1 className="font-bold text-3xl pt-2">29.6M</h1>
-              <p className="text-[10px] pt-2">21% more than last month</p>
-            </div>
-            <div>
-              <p className="text-[10px]">Total Reviews</p>
-              <h1 className="font-bold text-3xl pt-2">906K</h1>
-              <p className="text-[10px] pt-2">46% more than last month</p>
-            </div>
-            <div>
-              <p className="text-[10px]">Active Apps</p>
-              <h1 className="font-bold text-3xl pt-2">132+</h1>
-              <p className="text-[10px] pt-2">31 more will Launch</p>
-            </div>
+        <a href="https://www.apple.com/app-store/" target="_blank">
+          <img className="h-10" src={AppStore} alt="App Store" />
+        </a>
+      </div>
+      <div className="flex justify-center items-center text-center pt-10">
+        <img className="h-40 lg:h-80" src={Hero} alt="" />
+      </div>
+      <div className="bg-purple-600 text-center text-white pb-10">
+        <h1 className="font-semibold text-2xl pt-10">
+          Trusted by Millions, Built for You
+        </h1>
+        <div className="flex flex-col sm:flex-row text-center justify-center gap-10 sm:gap-36 pt-10">
+          <div>
+            <p className="text-[10px]">Total Downloads</p>
+            <h1 className="font-bold text-3xl pt-2">29.6M</h1>
+            <p className="text-[10px] pt-2">21% more than last month</p>
+          </div>
+          <div>
+            <p className="text-[10px]">Total Reviews</p>
+            <h1 className="font-bold text-3xl pt-2">906K</h1>
+            <p className="text-[10px] pt-2">46% more than last month</p>
+          </div>
+          <div>
+            <p className="text-[10px]">Active Apps</p>
+            <h1 className="font-bold text-3xl pt-2">132+</h1>
+            <p className="text-[10px] pt-2">31 more will Launch</p>
           </div>
         </div>
-        <div className="text-center">
-          <h1>Treanding Apps</h1>
-          <button onClick={() => navigate("/apps")} className="btn btn-primary">
-            Primary
-          </button>
+      </div>
+      <div className="max-w-6xl mx-auto text-center pt-10 px-4">
+        <h1 className="text-2xl md:text-3xl font-bold">Trending Apps</h1>
+        <p className="text-gray-500 mt-3 text-sm md:text-base">
+          Explore All Trending Apps on the Market developed by us
+        </p>
+        {/* Responsive Grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 pt-10">
+          {firstEightApps.map((item) => (
+            <div
+              key={item.id}
+              className="bg-white p-4 border border-gray-200 rounded-xl shadow-sm hover:shadow-lg transition w-full"
+            >
+              <img
+                className="rounded-xl w-full h-40 md:h-48 object-cover"
+                src={item.image}
+                alt={item.title}
+              />
+              <h5 className="mt-4 mb-2 text-base md:text-lg font-semibold">
+                {item.title}
+              </h5>
+              <p className="text-sm text-gray-600 line-clamp-2">
+                {item.description}
+              </p>
+            </div>
+          ))}
         </div>
+        <button
+          onClick={() => navigate("/apps")}
+          className="bg-blue-600 text-white px-6 py-2 rounded-lg mt-10 mb-10 hover:bg-blue-700 transition cursor-pointer"
+        >
+          Show More
+        </button>
       </div>
     </div>
   );
