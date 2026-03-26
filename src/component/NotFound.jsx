430a0c92690eea247aaac5f14b1d401135156098
Shimon Mondol <Shimonuap072@gmail.com>
2026-03-26 23:12:15 +0600

    nf2

--- src/component/NotFound.jsx
+++ src/component/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
   return (
     <div className="bg-[#F5F5F5] py-10 min-h-screen flex justify-center items-center">
       <div className="flex flex-col justify-center items-center text-center">
-        <img src={error} alt="404" className="w-80" />
+        <img src={error} alt="" className="w-80" />
         <h1 className="font-bold text-4xl mt-12">Page Not Found</h1>
       </div>
     </div>
