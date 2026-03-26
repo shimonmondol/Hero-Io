cb990141104527ab0c67f6647f34c33285af2d23
Shimon Mondol <Shimonuap072@gmail.com>
2026-03-26 21:43:18 +0600

    image-problem

--- src/component/NotFound.jsx
+++ src/component/NotFound.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import error from "../assets/error-404.png"
 
 const NotFound = () => {
   return (
-    <div className="bg-[#F5F5F5] py-10">
+    <div className="bg-[#F5F5F5] py-10 min-h-screen flex justify-center items-center">
       <div className="flex flex-col justify-center items-center text-center">
-        <img src={error} alt="" />
+        <img
+          src="https://i.ibb.co.com/QFj91zZj/error-404.png"
+          alt="404 Error"
+          className="w-80"
+        />
         <h1 className="font-bold text-4xl mt-12">Page Not Found</h1>
       </div>
     </div>
