81997ebde74df324f3f30f1187ce4e5052fdf1da
Shimon Mondol <Shimonuap072@gmail.com>
2026-03-26 22:22:51 +0600

    ibb

--- src/component/NotFound.jsx
+++ src/component/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     <div className="bg-[#F5F5F5] py-10 min-h-screen flex justify-center items-center">
       <div className="flex flex-col justify-center items-center text-center">
         <img
-          src="https://i.ibb.co.com/QFj91zZj/error-404.png"
+          src="https://i.ibb.co/QFj91zZj/error-404.png"
           alt="404 Error"
           className="w-80"
         />
